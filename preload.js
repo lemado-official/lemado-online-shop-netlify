9a11a0ab73a0519842acefac0625fb544deded35
lemado-official <lemado.official@gmail.com>
2026-07-01 03:35:26 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Frontend ichida ishlatish mumkin bo'lgan xavfsiz API'lar
 contextBridge.exposeInMainWorld('uLemdoAPI', {
-    // Kelajakda brauzer funksiyalari (masalan: tarix, xatchoplar) shu yerga yoziladi
-    getBrowserVersion: () => process.versions.electron
+    getBrowserVersion: () => process.versions.electron,
+    // Saytga o'tish funksiyasi
+    navigateTo: (url) => ipcRenderer.send('navigate-to', url)
 });
