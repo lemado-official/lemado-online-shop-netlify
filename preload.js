8359185f46f113b026cc60df57e97333b99448e3
lemado-official <lemado.official@gmail.com>
2026-07-01 04:00:09 -0500

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('uLemdoAPI', {
-    getBrowserVersion: () => process.versions.electron,
-    // Saytga o'tish funksiyasi
-    navigateTo: (url) => ipcRenderer.send('navigate-to', url)
-});
